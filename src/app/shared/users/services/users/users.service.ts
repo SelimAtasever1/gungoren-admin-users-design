4800a2e563789adae21d48353b26b46f67e25496
SelimAtasever1 <115715176+SelimAtasever1@users.noreply.github.com>
2023-07-22 00:13:05 +0300

    fix

--- src/app/shared/users/services/users/users.service.ts
+++ src/app/shared/users/services/users/users.service.ts
@@ -26,7 +26,7 @@ export class UsersService {
 
 		new UsersModel("moe doe", "24199214", "moedoe@mail.com", 
 		[
-			"https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403", 
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB_kvH-6eeKr0RbOziALLSz1Kchs3HvDFXGg&usqp=CAU", 
 			"https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403"
 		], "deactive"),
 
